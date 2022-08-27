ae41d42592db25ba75243c0be78fb6c04050cc12
MaestroDagan <ditisdagan@hotmail.com>
2022-08-27 18:23:06 +0200

    Simplified the package

--- src/index.js
+++ src/index.js
@@ -1,60 +1,68 @@
+const { randomBytes } = require("crypto");
+
 module.exports =
   /**
    *
    * @returns {string} UUID
    */
   () => {
-    try {
-      const OPTIONS = [
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f",
-        "g",
-        "h",
-        "i",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "o",
-        "p",
-        "q",
-        "r",
-        "s",
-        "t",
-        "u",
-        "v",
-        "w",
-        "x",
-        "y",
-        "z",
-        "0",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-      ];
+    // try {
+    //   const OPTIONS = [
+    //     "a",
+    //     "b",
+    //     "c",
+    //     "d",
+    //     "e",
+    //     "f",
+    //     "g",
+    //     "h",
+    //     "i",
+    //     "j",
+    //     "k",
+    //     "l",
+    //     "m",
+    //     "n",
+    //     "o",
+    //     "p",
+    //     "q",
+    //     "r",
+    //     "s",
+    //     "t",
+    //     "u",
+    //     "v",
+    //     "w",
+    //     "x",
+    //     "y",
+    //     "z",
+    //     "0",
+    //     "1",
+    //     "2",
+    //     "3",
+    //     "4",
+    //     "5",
+    //     "6",
+    //     "7",
+    //     "8",
+    //     "9",
+    //   ];
 
-      const resultArray = [];
+    //   const resultArray = [];
 
-      for (let index = 1; index < 37; index++) {
-        if (index == 9 || index == 14 || index == 19 || index == 24) {
-          resultArray.push("-");
-        } else
-          resultArray.push(OPTIONS[Math.floor(Math.random() * OPTIONS.length)]);
-      }
+    //   for (let index = 1; index < 37; index++) {
+    //     if (index == 9 || index == 14 || index == 19 || index == 24) {
+    //       resultArray.push("-");
+    //     } else
+    //       resultArray.push(OPTIONS[Math.floor(Math.random() * OPTIONS.length)]);
+    //   }
 
-      return resultArray.join("");
-    } catch (err) {
-      if (err) throw new Error(err);
-    }
+    //   return resultArray.join("");
+    // } catch (err) {
+    //   if (err) throw new Error(err);
+    // }
+
+    return `${randomBytes(4).toString("hex")}-${randomBytes(2).toString(
+      "hex"
+    )}-${randomBytes(2).toString("hex")}-${randomBytes(2).toString(
+      "hex"
+    )}-${randomBytes(6).toString("hex")}`;
   };
